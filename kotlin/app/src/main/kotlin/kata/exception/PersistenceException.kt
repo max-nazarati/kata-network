03fe9c10101a440d05eb1ac5fa9fb2e2db6ea8c3
max-nazarati <53535394+max-nazarati@users.noreply.github.com>
2024-09-25 19:38:45 +0200

    kotlin: made exception response more readable

--- kotlin/app/src/main/kotlin/kata/exception/PersistenceException.kt
+++ kotlin/app/src/main/kotlin/kata/exception/PersistenceException.kt
@@ -2,5 +2,5 @@ package kata.exception
 
 import kata.exception.ErrorKey
 
-class PersistenceException(val errorKey: ErrorKey, context: List<String> = listOf("")) : RuntimeException() {
+class PersistenceException(val errorKey: ErrorKey, val context: List<String> = listOf("")) : RuntimeException() {
 }
--- kotlin/app/src/main/kotlin/kata/exception/presentation/PersistenceExceptionDto.kt
+++ kotlin/app/src/main/kotlin/kata/exception/presentation/PersistenceExceptionDto.kt
@@ -1,4 +1,8 @@
 package kata.exception.presentation
 
-class PersistenceExceptionDto {
+import kata.exception.ErrorKey
+import kata.exception.PersistenceException
+
+data class PersistenceExceptionDto(val errorKey: ErrorKey, val context: List<String> = listOf("")) {
+    constructor(errorKey: PersistenceException) : this(errorKey.errorKey, errorKey.context)
 }
